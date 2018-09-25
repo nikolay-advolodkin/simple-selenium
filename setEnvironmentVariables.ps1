4da62d0128ba6c394e246ef02316e3c43a3ae841
Nikolay <40436850+nikolay-advolodkin@users.noreply.github.com>
2018-09-25 14:44:27 -0400

    Update setEnvironmentVariables.ps1

--- setEnvironmentVariables.ps1
+++ setEnvironmentVariables.ps1
@@ -6,7 +6,7 @@ Write-Host "sauce.userName that was passed in from Azure DevOps=>$sauceUserName"
 Write-Host "sauce.accessKey that was passed in from Azure DevOps=>$sauceAccessKey"
 
 [Environment]::SetEnvironmentVariable("SAUCE_USERNAME", "$sauceUserName", "User")
-[Environment]::SetEnvironmentVariable("SAUCE_KEY", "$sauceAccessKey)", "User")
-Write-Host "Here are the variables that were stored in SAUCE_KEY=>"$Env:SAUCE_KEY
+[Environment]::SetEnvironmentVariable("SAUCE_ACCESS_KEY", "$sauceAccessKey)", "User")
+Write-Host "Here are the variables that were stored in SAUCE_ACCESS_KEY=>"$Env:SAUCE_ACCESS_KEY
 Write-Host "Here are the variables that were stored=>"$Env:SAUCE_ACCESS_KEY
 Write-Host "Here are the variables that were stored in SAUCE_USERNAME=>"$Env:SAUCE_USERNAME
